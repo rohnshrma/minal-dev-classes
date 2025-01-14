00fb08aeda71b0aaa0a6aabfdcb51cfd38c9b39b
rohnshrma <rohn11shrma@gmail.com>
2025-01-14 22:34:39 +0530

    Add initial HTML and JavaScript files for JS introduction

---
+++ js intro/script.js
@@ -0,0 +1,54 @@
+// output :
+
+// console.log("hello world");
+// console.log(document);
+
+// alert | window.alert()
+
+// alert("hello world");
+
+// document.write("hello world");
+
+// prompt("Enter your name :", "john doe"); // message , default
+
+// var age; // declaration
+// age = 23; // initialization
+// age = 100;
+
+// var age = 23;
+
+// console.log(age);
+
+// 1. no space
+// 2. a variable name cannot start with a number but can include or end with a number
+// 3. a variable name cannot start include or end with a special symbol, except "_" and "$"
+// 4. do not use reserved keywords.
+
+// var prompt = "hello minal";
+
+// prompt("Enter your info : ");
+
+// var helloworldmynameis;
+// var helloWorldMyNameIs; //camelCasing
+// var hello_world_my_name_is; //snake_Casing
+
+// var a = 12;
+// var A = 34;
+// console.log(a, A);
+
+// var x;
+
+// console.log(x);
+
+// var x = null;
+// // console.log(x);
+
+// console.log(typeof x);
+
+var a = 10;
+var b = a;
+console.log(a, b);
+
+a = 30;
+
+console.log(a, b);
