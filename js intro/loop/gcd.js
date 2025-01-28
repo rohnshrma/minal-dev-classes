ac4e293137cd0f9d9d9ef6451bc277e5941ccf00
raawan <minalfulzele@gmail.com>
2025-01-28 16:47:23 +0100

    formatting

--- js intro/loop/gcd.js
+++ js intro/loop/gcd.js
@@ -2,17 +2,17 @@ const rl = require("readline-sync");
 const num1 = parseInt(rl.question("Enter Number 1 :: "));
 const num2 = parseInt(rl.question("Enter Number 2 :: "));
 
-var smallNum ;
+var smallNum;
 if (num1 > num2) {
-smallNum = num2
+  smallNum = num2;
 } else {
-    smallNum = num1
+  smallNum = num1;
 }
 
 var ans = 1;
-for (var i = 2; i <=smallNum / 2; i++) {
-    if(num1%i==0 && num2%i==0) {
-        ans=i
-    }
+for (var i = 2; i <= smallNum / 2; i++) {
+  if (num1 % i == 0 && num2 % i == 0) {
+    ans = i;
+  }
 }
-console.log(ans)
+console.log(ans);
--- js intro/loop/sum2Darray.js
+++ js intro/loop/sum2Darray.js
@@ -9,8 +9,8 @@ console.log(int2DArr[0].length);
 console.log(int2DArr[0][0]);
 
 var ans = 0;
-for (let i = 0; i < int2DArr.length; i++) {
-  for (let j = 0; j < int2DArr[i].length; j++) {
+for (var i = 0; i < int2DArr.length; i++) {
+  for (var j = 0; j < int2DArr[i].length; j++) {
     ans += int2DArr[i][j];
   }
 }
