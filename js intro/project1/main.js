1c6a5e02fb720905e383de251be426c59f924c69
raawan <minalfulzele@gmail.com>
2025-02-18 16:57:36 +0100

    removed commented out code

--- js intro/project1/main.js
+++ js intro/project1/main.js
@@ -4,16 +4,6 @@ import * as hof from "./functions.js";
 
 const numOfItemsToFetch = 20;
 
-// var tasks = new Array();
-// fetchTodos().then(
-//     data => {
-//         data.forEach(item => {
-//             tasks.push(new Task(item.id,item.title,item.completed))
-//         });
-//         console.log(tasks)
-//     }
-// )
-
 //Milestone 3 : Convert API response into an array of Task objects
 var tasks2 = await api.fetchTodos(numOfItemsToFetch);
 var taskArray = new Array();
@@ -21,8 +11,8 @@ tasks2.forEach((item) => {
   taskArray.push(new Task(item.id, item.title, item.completed));
 });
 
-//hof.displayTask(taskArray);
-// console.log("--------------------------------")
+hof.displayTask(taskArray);
+console.log("--------------------------------")
 hof.processTasks(hof.filterCompletedTasks, taskArray);
 console.log("--------------------------------");
 hof.processTasks(hof.extractTaskTitles, taskArray);
