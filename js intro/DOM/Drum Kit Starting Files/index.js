fa0bc88287769712efe13fe28f6825fbeff578a8
rohnshrma <rohn11shrma@gmail.com>
2025-02-21 22:21:47 +0530

    Refactor Drum Kit event handling and sound playback logic for improved readability and efficiency

--- js intro/DOM/Drum Kit Starting Files/index.js
+++ js intro/DOM/Drum Kit Starting Files/index.js
@@ -1,55 +1,40 @@
-var drums = document.querySelectorAll(".drum");
-drums.forEach((drum) => {
-  drum.addEventListener("click", (e) => {
-    const key = e.target.textContent;
-    playSound(key);
-    animate(key);
-  });
-});
+const playSound = (key) => {
+  const sounds = {
+    w: "crash",
+    a: "kick-bass",
+    s: "snare",
+    d: "tom-1",
+    j: "tom-2",
+    k: "tom-3",
+    l: "tom-4",
+  };
 
-document.addEventListener("keydown", (e) => {
-  const key = e.key;
-  playSound(key);
-  animate(key);
-});
-
-function playSound(key) {
-  switch (key) {
-    case "w":
-      var audio = new Audio("./sounds/crash.mp3");
-      audio.play();
-      break;
-    case "a":
-      var audio = new Audio("./sounds/kick-bass.mp3");
-      audio.play();
-      break;
-    case "s":
-      var audio = new Audio("./sounds/snare.mp3");
-      audio.play();
-      break;
-    case "d":
-      var audio = new Audio("./sounds/tom-1.mp3");
-      audio.play();
-      break;
-    case "j":
-      var audio = new Audio("./sounds/tom-2.mp3");
-      audio.play();
-      break;
-    case "k":
-      var audio = new Audio("./sounds/tom-3.mp3");
-      audio.play();
-      break;
-    case "l":
-      var audio = new Audio("./sounds/tom-4.mp3");
-      audio.play();
-      break;
-  }
-}
+  if (sounds[key]) console.log(`./sounds/${sounds[key]}.mp3`);
+  if (sounds[key]) new Audio(`./sounds/${sounds[key]}.mp3`).play();
+};
 
 function animate(key) {
   var drum = document.querySelector(`.${key}`);
-  drum.classList.add("pressed");
-  setTimeout(() => {
-    drum.classList.remove("pressed");
-  }, 100);
+  if (drum) {
+    drum.classList.add("pressed");
+    setTimeout(() => drum.classList.remove("pressed"), 100);
+  }
 }
+
+const handleEvent = (key) => {
+  key = key.toLowerCase().trim();
+
+  var validKeys = ["w", "a", "s", "d", "j", "k", "l"];
+  if (validKeys.includes(key)) {
+    playSound(key);
+    animate(key);
+  }
+};
+
+document
+  .querySelectorAll(".drum")
+  .forEach((drum) =>
+    drum.addEventListener("click", (e) => handleEvent(e.target.textContent))
+  );
+
+document.addEventListener("keydown", (e) => handleEvent(e.key));
